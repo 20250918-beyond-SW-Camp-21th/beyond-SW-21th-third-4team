a6434042221c8a4ddf658a341b8276c49989eb69
sekong11 <sekong1108@gmail.com>
2025-12-27 17:50:09 +0900

    feat: User Details 연동 (#24)

---
+++ backend/src/main/java/com/insilenceclone/backend/domain/user/security/CustomUser.java
@@ -0,0 +1,47 @@
+package com.insilenceclone.backend.domain.user.security;
+
+import com.insilenceclone.backend.domain.user.entity.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+public class CustomUser implements UserDetails {
+
+    private final User user;
+
+    public CustomUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        // Spring Security에서 username으로 쓰는 값 = loginId
+        return user.getLoginId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+
+    @Override
+    public boolean isEnabled() { return true; }
+}
---
+++ backend/src/main/java/com/insilenceclone/backend/domain/user/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.insilenceclone.backend.domain.user.security;
+
+import com.insilenceclone.backend.domain.user.entity.User;
+import com.insilenceclone.backend.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+        User user = userRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found = "+loginId));
+
+        return new CustomUser(user);
+    }
+}
