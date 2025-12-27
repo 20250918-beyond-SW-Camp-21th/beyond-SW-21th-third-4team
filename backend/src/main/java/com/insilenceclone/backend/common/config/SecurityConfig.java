fcfeaf8f2a28b65c65bd62eb978a7272529c8c49
sekong11 <sekong1108@gmail.com>
2025-12-27 22:33:24 +0900

    fix: [BE] formLogin과 htttpBasic disabled 처리 (#24)

--- backend/src/main/java/com/insilenceclone/backend/common/config/SecurityConfig.java
+++ backend/src/main/java/com/insilenceclone/backend/common/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import com.insilenceclone.backend.common.jwt.RestAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -43,7 +42,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
                 // 세션 사용 안함
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
