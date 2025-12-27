fa72fbddf3fb4800b9646fd40d98851d76f95a8b
yoocy0 <yoocy0328@gmail.com>
2025-12-27 14:45:40 +0900

    feat: [BE] ãProductImageRepository 생성 및 메소드 구현 (#28)

---
+++ backend/src/main/java/com/insilenceclone/backend/domain/productDetail/repository/ProductImageRepository.java
@@ -0,0 +1,15 @@
+package com.insilenceclone.backend.domain.productDetail.repository;
+
+import com.insilenceclone.backend.domain.productDetail.entity.ProductImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+
+    // 상품 이미지 조회
+    List<ProductImage> findByProductIdOrderBySortOrderAsc(Long productId);
+
+}
