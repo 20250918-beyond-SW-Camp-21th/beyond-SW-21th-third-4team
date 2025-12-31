d9d914ac08357b9c952c432d44217f4eb9a627bf
Ongsaem0 <tkdwnsghqkr1@gmail.com>
2025-12-31 17:41:37 +0900

    [FE]메인 페이지 베스트 컬렉션 구현 (#102)

--- frontend/src/views/main/HomeView.vue
+++ frontend/src/views/main/HomeView.vue
@@ -1,19 +1,19 @@
 <template>
   <main>
-    <!-- 3. 여기에 배너 부품을 갖다 씁니다 -->
+    <!-- 1. 메인 배너 -->
     <MainBanner />
 
-    <!-- 그 외 메인 페이지의 다른 내용들이 아래에 들어갑니다 -->
-    <div style="text-align: center; padding: 50px;">
-      <h2>여기는 메인 페이지의 나머지 내용입니다.</h2>
-    </div>
+    <!-- 2. 베스트 상품 목록  -->
+    <BestProductList />
   </main>
 </template>
 
 <script setup>
-// 1. 아까 만든 부품 파일을 가져옵니다.
-// (경로 설명: @는 src 폴더를 의미:ㅈㅂ합니다. 혹은 ../../components/MainBanner.vue 로 해도 됩니다)
+// 메인페이지 중앙부 사진
 import MainBanner from '../../components/main/MainBanner.vue';
+
+// 메인페이지 중앙부 베스트 컬렉션
+import BestProductList from '../../components/main/BestProductList.vue';
 </script>
 
 <style scoped>
