2f8b784f5264d1087c18733219dad97da1d5e147
Ongsaem0 <tkdwnsghqkr1@gmail.com>
2025-12-31 10:19:48 +0900

    [BE]controller 어노테이션추가 (#83)

--- backend/src/main/java/com/insilenceclone/backend/domain/product/controller/ProductController.java
+++ backend/src/main/java/com/insilenceclone/backend/domain/product/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.insilenceclone.backend.domain.product.controller;
 import com.insilenceclone.backend.common.response.ApiResponse;
 import com.insilenceclone.backend.domain.product.dto.ProductResponseDto;
 import com.insilenceclone.backend.domain.product.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,12 +13,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
+@Tag(name = "상품 API", description = "상품 목록 조회 및 필터링 관련 API")
 public class ProductController {
 
     private final ProductService productService;
 
     // 카테고리 조회 API(ex. /api/v1/products?category=OUTER)
     @GetMapping
+    @Operation(summary = "상품 목록 조회", description = "카테고리별 상품을 조회하거나 전체 목록을 조회합니다.")
     public ApiResponse<List<ProductResponseDto>> getProductList(
             @RequestParam(value = "category", required = false) String category
     ) {
