d5ff4b8b093aad2ac150a823d5772d3c92678e63
sekong11 <sekong1108@gmail.com>
2026-01-02 00:40:24 +0900

    fix: [BE] 응다 (#108)

--- backend/src/main/java/com/insilenceclone/backend/domain/cart/dto/response/CartItemResponseDto.java
+++ backend/src/main/java/com/insilenceclone/backend/domain/cart/dto/response/CartItemResponseDto.java
@@ -1,6 +1,7 @@
 package com.insilenceclone.backend.domain.cart.dto.response;
 
 public record CartItemResponseDto (
+        Long cartItemId,
         Long productId,
         String name,
         Long price,
